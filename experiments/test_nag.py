8bd664d774ef6d40623c303d197d240521e6b5d1
weathon <wguo6358@gmail.com>
2025-07-17 17:05:05 -0700

    rm font

--- experiments/test_nag.py
+++ experiments/test_nag.py
@@ -24,7 +24,7 @@ with open("../prompts/test_prompts.json.new", "r") as f:
 
 def run(nag_scale, nag_alpha, nag_tau):
     wandb.init(project="nag-sweep")
-    score = 0
+    score = np.array([0, 0], dtype=int)
     total = 0
     for seed in range(1):
         for i in dev_prompts:
@@ -48,7 +48,9 @@ def run(nag_scale, nag_alpha, nag_tau):
                 font = ImageFont.truetype("DejaVuSans.ttf", 50)
                 text = f"{delta}, -: {i['missing_element']}"
                 draw.text((10, 10), text, fill="white", font=font)
-                wandb.log({"score": score/total, "img": wandb.Image(image, caption=f"+: {i['prompt']}\n -: {i['missing_element']}")})
+                text = f"{delta[0]}, {delta[1]}, -: {i['missing_element']}"
+                draw.text((10, 10), text, fill="white")
+                wandb.log({"pos_score":score[0]/total, "neg_score":score[1]/total, "img": wandb.Image(image, caption=f"+: {i['prompt']}\n -: {i['missing_element']}")})
             else:
                 wandb.log({"img": wandb.Image(image, caption=f"+: {i['prompt']}\n -: {i['missing_element']}")})
 
