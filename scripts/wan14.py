7af4b75ad7b3e194d2ac2ddb4b2e5c70b2801009
weathon <wguo6358@gmail.com>
2025-07-22 02:37:50 -0700

    quality seems working

--- scripts/wan14.py
+++ scripts/wan14.py
@@ -46,32 +46,35 @@ pipeline.load_lora_weights(
 
 pipeline.to("cuda")
 
-prompt = """
-The camera rushes from far to near in a low-angle shot, 
-revealing a white ferret on a log. It plays, leaps into the water, and emerges, as the camera zooms in 
-for a close-up. Water splashes berry bushes nearby, while moss, snow, and leaves blanket the ground. 
-Birch trees and a light blue sky frame the scene, with ferns in the foreground. Side lighting casts dynamic 
-shadows and warm highlights. Medium composition, front view, low angle, with depth of field.
-"""
+old_prompt = "CG animation style, a small blue bird takes off from the ground, flapping its wings. The bird's feathers are delicate, with a unique pattern on its chest. The background shows a blue sky with white clouds under bright sunshine. The camera follows the bird upward, capturing its flight and the vastness of the sky from a close-up, low-angle perspective."
 
-neg_prompt = "low quality video, blurry, distorted, low resolution, weird motion"
+prompt = (
+    "An astronaut hatching from an egg, on the surface of the moon, the darkness and depth of space realised in "
+    "the background. High quality, ultrarealistic detail and breath-taking movie-like camera shot."
+)
+
+old_neg_prompt = "low quality video, blurry, distorted, low resolution, weird motion"
+neg_prompt = "low quality, blurry, distorted, low resolution, nnatural motion, unnatural lighting"
 height = 480
 width = 832
-frames = 33 
+frames = 31 
 
+prompt_len_ratio = len(pipeline.tokenizer.tokenize(prompt, padding=False, truncation=True, max_length=512))/len(pipeline.tokenizer.tokenize(neg_prompt, padding=False, truncation=True, max_length=512))
+# print(len(pipeline.tokenizer.tokenize(old_prompt, padding=False, truncation=True, max_length=512))/len(pipeline.tokenizer.tokenize(old_neg_prompt, padding=False, truncation=True, max_length=512)))
+# 5.18/2.04=2.54
 
 neg_prompt_embeds, _ = pipeline.encode_prompt(
     prompt=neg_prompt,
     padding=False,
     do_classifier_free_guidance=False,
-)
+) 
 
 pos_prompt_embeds, _ = pipeline.encode_prompt( 
     prompt=prompt,
     do_classifier_free_guidance=False, 
     max_sequence_length=512 - neg_prompt_embeds.shape[1],
 )
-pipeline.set_adapters("lora", 0.45)
+pipeline.set_adapters("lora", 0.4)
 
 
 
@@ -82,10 +85,10 @@ print(neg_len, pos_len)
 img_len = (height//8) * (width//8) * 3 * (frames // 4 + 1) // 12
 print(img_len)
 mask = torch.zeros((1, img_len, pos_len+neg_len)).cuda()
-mask[:, :, -neg_len:] = -0.15
+mask[:, :, -neg_len:] = -0.3
 
 for block in pipeline.transformer.blocks:
-    block.attn2.processor = WanAttnProcessor2_0(scale=1, neg_prompt_length=neg_len, attn_mask=mask)
+    block.attn2.processor = WanAttnProcessor2_0(scale=0.3/prompt_len_ratio, neg_prompt_length=neg_len, attn_mask=mask)
 
 prompt_embeds = torch.cat([pos_prompt_embeds, neg_prompt_embeds], dim=1)
 
@@ -95,9 +98,31 @@ output = pipeline(
     height=height,
     width=width,
     num_frames=frames,
-    num_inference_steps=8,
+    num_inference_steps=6,
     guidance_scale=0.0, 
-    generator=torch.Generator(device="cuda").manual_seed(42),
+    generator=torch.Generator(device="cuda").manual_seed(8384),
 ).frames[0]
 # save video with video_id 3 digits
 export_to_video(output[5:], f"14b_vsf.mp4", fps=15)
+
+
+mask = torch.zeros((1, img_len, pos_len+neg_len)).cuda()
+mask[:, :, -neg_len:] = -torch.inf 
+
+for block in pipeline.transformer.blocks:
+    block.attn2.processor = WanAttnProcessor2_0(scale=0.8, neg_prompt_length=neg_len, attn_mask=mask)
+
+prompt_embeds = torch.cat([pos_prompt_embeds, neg_prompt_embeds], dim=1)
+
+output = pipeline(
+    prompt_embeds=prompt_embeds,
+    negative_prompt=neg_prompt,
+    height=height,
+    width=width,
+    num_frames=frames,
+    num_inference_steps=6,
+    guidance_scale=0.0, 
+    generator=torch.Generator(device="cuda").manual_seed(8384),
+).frames[0]
+# save video with video_id 3 digits
+export_to_video(output[5:], f"14b_original.mp4", fps=15)
