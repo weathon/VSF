f8df266473848668adeb85be0582a645e1bbd2db
weathon <wguo6358@gmail.com>
2025-07-25 15:46:17 -0700

    finished

--- experiments/runtime/run.py
+++ experiments/runtime/run.py
@@ -6,8 +6,10 @@ import subprocess
 
 files = os.listdir(".")
 files = [f for f in files if f.endswith(".py") and f != "run.py"]
-
+finished = "nag_wan.py"
 import time
 for f in files:
+    if f == finished:
+        continue
     subprocess.run(['python3', f])
     time.sleep(60 * 5)
