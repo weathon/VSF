8f6a78ef6fb2e9fea44d32c1cd5a8911335ca2be
weathon <wguo6358@gmail.com>
2025-07-25 15:51:53 -0700

    remove double loop

--- experiments/runtime/none_sd3.py
+++ experiments/runtime/none_sd3.py
@@ -24,20 +24,18 @@ import time
 total_time = 0
 count = 0
 wandb.init(project="compute", name="none_sd3")
+
 for i in dev_prompts[:25]:
-    score = np.array([0, 0], dtype=int)
-    total = 0
-    for i in dev_prompts[:25]:
-        time_start = time.time()
-        image = pipe(
-            i["prompt"],
-            guidance_scale=0.,
-            num_inference_steps=8,
-            generator=torch.Generator("cuda").manual_seed(0),
-        ).images[0]
-        time_end = time.time()
-        total_time += (time_end - time_start)
-        count += 1
-        wandb.log({"time_per_image": total_time / count})
+    time_start = time.time()
+    image = pipe(
+        i["prompt"],
+        guidance_scale=0.,
+        num_inference_steps=8,
+        generator=torch.Generator("cuda").manual_seed(0),
+    ).images[0]
+    time_end = time.time()
+    total_time += (time_end - time_start)
+    count += 1
+    wandb.log({"time_per_image": total_time / count})
 wandb.log({"peak_memory": torch.cuda.max_memory_allocated() / (1024 ** 3)})
 
