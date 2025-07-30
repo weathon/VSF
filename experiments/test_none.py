401d17ddb0011c4f85c3502cc6acbf080690f6ab
weathon <wguo6358@gmail.com>
2025-07-29 21:22:18 -0700

    add none baseline

---
+++ experiments/test_none.py
@@ -0,0 +1,50 @@
+import torch
+import sys
+from nag import NAGStableDiffusion3Pipeline
+import json
+import judge
+import wandb
+import numpy as np
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description="Run NAG sweep")
+parser.add_argument("--eval_later", action="store_true", help="Run evaluation later")
+args = parser.parse_args()
+        
+model_id = "stabilityai/stable-diffusion-3.5-large-turbo"
+pipe = NAGStableDiffusion3Pipeline.from_pretrained(
+    model_id,
+    torch_dtype=torch.bfloat16,
+)
+pipe.to("cuda")
+
+with open("../prompts/test_prompts.json.new", "r") as f:
+    dev_prompts = json.load(f)
+
+def run(nag_scale, nag_alpha, nag_tau):
+    wandb.init(project="nag-sweep")
+    score = np.array([0, 0, 0], dtype=float)
+    total = 0
+    for seed in range(2):
+        for i in dev_prompts:
+            image = pipe(
+                i["prompt"],
+                guidance_scale=0.,
+                nag_scale=0.0, 
+                num_inference_steps=8,
+                generator=torch.Generator("cuda").manual_seed(seed),
+            ).images[0]
+            if not args.eval_later:
+                delta = judge.vqa(image, i["question_1"], i["question_2"])
+                score += delta
+                total += 1
+                # show the score as text on the image using PIL
+                from PIL import ImageDraw, ImageFont
+                wandb.log({"pos_score_overall":score[0]/total, "neg_score_overall":score[1]/total, "quality_score_overall": score[2]/total,"img": wandb.Image(image, caption=f"+: {i['prompt']}\n -: {i['missing_element']}"), 
+                          "pos_score": delta[0], "neg_score": delta[1], "quality_score": delta[2]})
+            else:
+                wandb.log({"img": wandb.Image(image, caption=f"+: {i['prompt']}\n -: {i['missing_element']}")})
+
+run(11, 0.5, 5)
+# run(4, 0.125, 2.5)
