97d09811d790d62370c9cc525adda07240bbfae0
weathon <wguo6358@gmail.com>
2025-09-07 03:03:45 +0000

    flux

--- flux_demo.py
+++ flux_demo.py
@@ -8,17 +8,17 @@ pipe = VSFFluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch
 # prompt = "a canadian winter landscape in the style of a 19th century painting"
 prompt = "a bike leaning on a broken wall in the style of a 19th century painting"
 images = []
-for scale in np.arange(2.0, 3.0, 0.1):
-    image = pipe(
-        prompt,
-        negative_prompt="wheels",
-        guidance_scale=0.0,
-        num_inference_steps=8, 
-        max_sequence_length=256,
-        offset=0.2,
-        scale=scale, 
-        generator=torch.Generator("cpu").manual_seed(93)
-    ).images[0]
-    images.append(image)
+# for scale in np.arange(2.0, 3.0, 0.1):
+image = pipe(
+    prompt,
+    negative_prompt="wheels",
+    guidance_scale=0.0,
+    num_inference_steps=8, 
+    max_sequence_length=256,
+    offset=0.2,
+    scale=3.0, 
+    generator=torch.Generator("cpu").manual_seed(93)
+).images[0].save("flux_demo.png")
+#     images.append(image)
 
-imageio.mimsave("flux_demo.mp4", images, fps=4)
+# imageio.mimsave("flux_demo.mp4", images, fps=4)
